62df6460f570dbed36364e89593c7ddf54d39fe1
Umar Dzhumaza <106446440+umardzhumaza@users.noreply.github.com>
2022-09-14 13:12:41 +0600

    Solved 1079

---
+++ src/Week1/WeightedAverages1079.java
@@ -0,0 +1,19 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class WeightedAverages1079 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCount = sc.nextInt();
+        //(100 х 70) + (300 х 80) + (50 х 95)/450
+        float firstValue, secondValue, thirdValue, averages;
+        for (int i = 0; i < testCount; i++) {
+            firstValue = sc.nextFloat();
+            secondValue = sc.nextFloat();
+            thirdValue = sc.nextFloat();
+            averages = ((firstValue * 2) + (secondValue * 3) + (thirdValue * 5)) / 10;
+            System.out.printf("%.1f\n", averages);
+        }
+    }
+}
