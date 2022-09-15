c7d9fad26380d018dc62044ee15860e0b7e6eff7
Umar Dzhumaza <106446440+umardzhumaza@users.noreply.github.com>
2022-09-15 16:11:29 +0600

    Solved 1176

--- week2/FibonacciArray1176.java
+++ week2/FibonacciArray1176.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
 
 public class FibonacciArray1176 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        long[] fib = new long[61];
+        long value = 0;
+        int testCount = sc.nextInt();
+        while (testCount != 0) {
+            int number = sc.nextInt();
+            for (int i = 3; i <= 60; i++) {
+                fib[0] = 0;
+                fib[1] = 1;
+                fib[2] = 1;
+                value = fib[i - 1] + fib[i - 2];
+                fib[i] = value;
+            }
+            System.out.println("Fib(" + number + ") = " + fib[number]);
+            testCount--;
+        }
     }
 }
