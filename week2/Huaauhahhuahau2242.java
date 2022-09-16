c8301f17599196fc4559c79e74bdfcb2c2cd37d6
Umar Dzhumaza <106446440+umardzhumaza@users.noreply.github.com>
2022-09-16 14:28:30 +0600

    Solved 2242

--- week2/Huaauhahhuahau2242.java
+++ week2/Huaauhahhuahau2242.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CompareSubstring1237 {
+public class Huaauhahhuahau2242 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
