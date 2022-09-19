31f81de7c8ccb1026dae9d0b39a2e74d983ab9ba
Umar Dzhumaza <106446440+umardzhumaza@users.noreply.github.com>
2022-09-19 21:58:50 +0600

    REFACTOR CamelCase

--- week4/create.sql
+++ week4/create.sql
@@ -6,28 +6,28 @@ CREATE TABLE Car(
         id int AUTO_INCREMENT PRIMARY KEY,
         model varchar(50) NOT NULL,
         year_of_issue varchar(50) NOT NULL,
-        color varchar(50),
-        horse_power int,
+        color varchar(50) NOT NULL,
+        horse_power int NOT NULL,
         price int NOT NULL,
-        max_speed int
+        max_speed int NOT NULL
 );
 
 CREATE TABLE Buyer(
         id int AUTO_INCREMENT PRIMARY KEY,
         name varchar(255) NOT NULL,
         sur_name varchar(255) NOT NULL,
-        age int,
+        age int NOT NULL,
         gender char(1) NOT NULL,
-        address varchar(255),
-        email varchar(255),
+        address varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
         phone_number varchar(30) NOT NULL
 );
 
 CREATE TABLE Orders(
         id int AUTO_INCREMENT PRIMARY KEY,
-        buyer_id int,
-        car_id int,
-        total_amount int,
+        buyer_id int NOT NULL,
+        car_id int NOT NULL,
+        total_amount int NOT NULL,
         order_date varchar(50) NOT NULL,
         delivery_date varchar(50) NOT NULL,
         payment_method varchar(50) NOT NULL,
